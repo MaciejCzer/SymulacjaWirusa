c68b038d04533e51b6f6da6729c1fd20365183f7
MaciejCzer <m4ciekssspl@gmail.com>
2025-06-03 22:08:18 +0200

    Zarys symulacji poruszanie ludzi

--- src/main/java/Simulation/Simulation.java
+++ src/main/java/Simulation/Simulation.java
@@ -1,7 +1,48 @@
 package Simulation;
 
+import java.util.Random;
+
+
 public class Simulation {
+    private final MapGrid grid;
+    private final Config config;
+    private static final Random random = new Random();
+
+    public Simulation(Config config) {
+        this.config = config;
+        this.grid = new MapGrid(config.getMapWidth(), config.getMapHeight());
+        initializeSimulation();
+    }
+
+    private void initializeSimulation() {
+        for (int i = 0; i < config.getInitialPopulation(); i++) {
+            Point position = getRandomPosition();
+            Person person = new Person(position);
+            grid.addPerson(person);
+        }
+    }
+
+    private Point getRandomPosition() {
+        return new Point(
+                random.nextInt(config.getMapWidth()),
+                random.nextInt(config.getMapHeight()));
+    }
+
+    public void run() {
+        for (int epoch = 0; epoch < config.getSimulationDuration(); epoch++) {
+            grid.movePeople();
+            //---------------------------
+            for (Person person : grid.getPeople()) {
+                Point pos = person.getPosition();
+                System.out.println(pos.getX() + " " + pos.getY());
+
+            }
+            //^to tylko zeby zobaczyc czy dziala^
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Config config = Config.getDefaultConfig();
+        Simulation simulation = new Simulation(config);
+        simulation.run();
     }
 }
