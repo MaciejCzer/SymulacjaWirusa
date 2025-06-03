5005186437e5c78181765268f3157e1d7d898374
MaciejCzer <m4ciekssspl@gmail.com>
2025-06-04 00:27:52 +0200

    funkcja do dystansu

--- src/main/java/Simulation/Point.java
+++ src/main/java/Simulation/Point.java
@@ -7,6 +7,11 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
+    public double distance(Point other) {
+        return Math.sqrt(Math.pow(other.x - this.x, 2) + Math.pow(other.y - this.y, 2));
+    }
+
     public int getX() { return x; }
     public int getY() { return y; }
 
