cb53193ec759eb6bbd4a9f119a615bd6637be6ed
antoniwegrzyn <ps4antek@wp.pl>
2025-06-04 19:50:25 +0200

    Virus.java

---
+++ src/main/java/Simulation/Hospital.java
@@ -0,0 +1,29 @@
+package Simulation;
+
+public class Hospital {
+
+    private Point position;
+    private int healingRdius;
+
+    public Hospital(Point position, int healingRdius) {
+        this.position = position;
+        this.healingRdius = healingRdius;
+    }
+    public Point getPosition() {
+        return position;
+    }
+    public int getHealingRdius() {
+        return healingRdius;
+
+    }
+
+    public boolean canHeal(Person person) {
+        return position.distance(person.getPosition()) <= healingRdius;
+
+    }
+    public boolean heal(Person person) {
+        if (person.isInfected()){
+            person.heal();
+        }
+    }
+}
