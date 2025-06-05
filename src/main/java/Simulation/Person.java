7f199852bd640717d3cc260529bfdc6ce1ed6a57
MaciejCzer <m4ciekssspl@gmail.com>
2025-06-05 11:38:41 +0200

    funkcje infekowania odpornosci

--- src/main/java/Simulation/Person.java
+++ src/main/java/Simulation/Person.java
@@ -4,7 +4,10 @@ import java.util.Random;
 
 public class Person {
     private Point position;
+    private boolean infected;
     private boolean alive;
+    private boolean immune;
+    private int immunityTime;
     private static final Random rand = new Random();
     public Person(Point position) {
         this.position = position;
@@ -21,6 +24,32 @@ public class Person {
         position = new Point(newX, newY);
     }
 
+    public void infect() {
+        this.infected = true;
+    }
+
+    public void heal() {
+        this.infected = false;
+        this.immune = true;
+        this.immunityTime = 0;
+    }
+
+    public void die() {
+        this.alive = false;
+    }
+
+    public void addImmunityTime() {
+            immunityTime++;
+    }
+
+    public void removeImmunity() {
+        this.immune = false;
+        this.immunityTime = 0;
+    }
+
     public Point getPosition() { return position; }
     public boolean isAlive() { return alive; }
+    public boolean isInfected() { return infected; }
+    public boolean isImmune() { return immune; }
+    public int getImmunityTime() { return immunityTime; }
 }
