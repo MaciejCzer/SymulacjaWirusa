a7f8e71378718044331f57316844a2f1a563507b
MaciejCzer <m4ciekssspl@gmail.com>
2025-06-05 11:39:08 +0200

    symulacja

--- src/main/java/Simulation/Simulation.java
+++ src/main/java/Simulation/Simulation.java
@@ -10,7 +10,7 @@ public class Simulation {
 
     public Simulation(Config config) {
         this.config = config;
-        this.grid = new MapGrid(config.getMapWidth(), config.getMapHeight());
+        this.grid = new MapGrid(config.getMapWidth(), config.getMapHeight(), config);
         initializeSimulation();
     }
 
@@ -18,7 +18,16 @@ public class Simulation {
         for (int i = 0; i < config.getInitialPopulation(); i++) {
             Point position = getRandomPosition();
             Person person = new Person(position);
+            if (i < config.getInitialInfected()) {
+                person.infect();
+            }
             grid.addPerson(person);
+        }
+
+        for (int i = 0; i < config.getHospitalsNumber(); i++) {
+            Point position = getRandomPosition();
+            Hospital hospital = new Hospital(position, config.getHealingRdius());
+            grid.addHospital(hospital);
         }
     }
 
@@ -31,13 +40,16 @@ public class Simulation {
     public void run() {
         for (int epoch = 0; epoch < config.getSimulationDuration(); epoch++) {
             grid.movePeople();
-            //---------------------------
+            grid.infectPeople();
+            grid.healPeople();
+            grid.deaths();
+            grid.updateTimer();
+            System.out.println("Epoka"+ epoch);
             for (Person person : grid.getPeople()) {
                 Point pos = person.getPosition();
                 System.out.println(pos.getX() + " " + pos.getY());
-
+                System.out.println("Zyje " + person.isAlive() + " Chory " + person.isInfected() + " odporny " + person.isImmune());
             }
-            //^to tylko zeby zobaczyc czy dziala^
         }
     }
     public static void main(String[] args) {
